c7d3f84ca74c7fccd4f32c9786ba3eb5d7307b8a
bjoern <bjoern@hoehrmann.de>
2024-05-20 21:44:47 +0200

    Use chat API instead, Llama3, both -text and -instruct, get too confused without the role tokens. And also actually implement iterating over the pages.

--- IETF-CERT-template.md
+++ IETF-CERT-template.md
@@ -47,11 +47,11 @@ Commands MUST be terminated by a newline character. After issuing a command, you
 
 ## Fields
 
-- `Summary of page X: [Your summary of the current page here]`: Your concise summary of the current page. Summaries are MANDATORY for each page, after retrieving a page you MUST provide your summary of it.
-- `Note to self: [jot your thoughts here, take notes]`: Use this field to scribble into the margins of the document, notes only for yourself, e.g. to keep track of something you may want to investigate later when having read more of the document. Not shared, visible only to you.
+- `Summary of page X: [Your summary of the current page here]`: Your concise summary of the current page. You MUST provide the REQUIRED summary for each page. It is imperative to follow guidelines when writing a summary.
+- `Note to self: [jot your thoughts here, take notes]`: Use this field to scribble into the margins of the document, notes only for yourself, e.g. to keep track of something you may want to investigate later when having read more of the document. Not shared, private and visible only to you.
 - `Comment: [Your expert review comment here]`: This adds your official expert review comment to the current page of the draft under review.
 
-Field names are REQUIRED. Fields MUST be terminated by an empty line.
+Two newlines (an empty line) are required after a field.
 
 ## Usage instructions
 
@@ -65,62 +65,4 @@ This is all you need to know about the terminal. The terminal will launch now. A
 
 Terminal active. This is not a simulation. You are the Expert Reviewer. Follow the terminal usage instructions closely. Fill in fields, use commands, do not comment on your own use of the terminal or on the review process itself.
 
-You are asked to review the Internet-Draft `draft-ietf-tsvwg-sctp-zero-checksum-10` titled "Zero Checksum for the Stream Control Transmission Protocol" (17 pages). To begin your review, issue the command to retrieve the first page and await the response. Follow the Usage instructions provided to you earlier. After retrieving all pages, you will be given further instructions.
-
-$ page 1
-
-> Network Working Group                                           M. Tüxen
-> Internet-Draft                           Münster Univ. of Appl. Sciences
-> Intended status: Standards Track                               V. Boivie
-> Expires: 19 November 2024                                    F. Castelli
->                                                                   Google
->                                                                 R. Jesup
->                                                                  Mozilla
->                                                              18 May 2024
-> 
-> 
->        Zero Checksum for the Stream Control Transmission Protocol
->                  draft-ietf-tsvwg-sctp-zero-checksum-10
-> 
-> Abstract
-> 
->    The Stream Control Transmission Protocol (SCTP) uses a 32-bit
->    checksum in the common header of each packet to provide some level of
->    data integrity.  If another method used by SCTP already provides the
->    same or a higher level of data integrity, computing this checksum
->    does not provide any additional protection, but does consume
->    computing resources.
-> 
->    This document provides a simple extension allowing SCTP to save these
->    computing resources by using zero as the checksum in a backwards
->    compatible way.  It also defines how this feature can be used when
->    SCTP packets are encapsulated in Datagram Transport Layer Security
->    (DTLS) packets.
-> 
-> Status of This Memo
-> 
->    This Internet-Draft is submitted in full conformance with the
->    provisions of BCP 78 and BCP 79.
-> 
->    Internet-Drafts are working documents of the Internet Engineering
->    Task Force (IETF).  Note that other groups may also distribute
->    working documents as Internet-Drafts.  The list of current Internet-
->    Drafts is at https://datatracker.ietf.org/drafts/current/.
-> 
->    Internet-Drafts are draft documents valid for a maximum of six months
->    and may be updated, replaced, or obsoleted by other documents at any
->    time.  It is inappropriate to use Internet-Drafts as reference
->    material or to cite them other than as "work in progress."
-> 
->    This Internet-Draft will expire on 19 November 2024.
-> 
-> 
-> 
-> 
-> 
-> 
-> 
-> Tüxen, et al.           Expires 19 November 2024                [Page 1]
-
-<!-- End of page 1. There are 16 more pages you must read. Use the command `$ page 2` to proceed to the next page and then stop, halt, terminate -->
-
+You are asked to review the Internet-Draft draft-ietf-tsvwg-sctp-zero-checksum-10 titled "Zero Checksum for the Stream Control Transmission Protocol" (17 pages). To begin your review, issue the command to retrieve the first page and await the response. Follow the Usage instructions provided to you earlier. After retrieving all pages, you will be given further instructions.
--- IETF-CERT.py
+++ IETF-CERT.py
@@ -1,73 +1,249 @@
+import collections
+import re
 import sys
 import click
 from llama_cpp import Llama, LlamaGrammar, StoppingCriteriaList
+import rich
+from itertools import accumulate
+
+
+def read_draft(draft: click.File):
+    pages = draft.read().rstrip("\r\n\t\f ").split("\f")
+
+    def quote(page):
+        quoted = "\n".join(["> " + line for line in page.split("\n")])
+        quoted = quoted.rstrip(">\r\n\t\f ")
+        quoted += (
+            "\n\n"
+            + "<!-- End of page. Start with your expert review summary! "
+            + "Then proceed as instructed earlier! There are more pages you must read "
+            + "and review. Use the command `$ page N` to proceed to "
+            + "the next page. -->\n"
+        )
+        return quoted
+
+    return [[{"type": "draft", "text": quote(page)}] for page in pages]
+
+
+# def takewhile_it_fits(iterator, weight_func, capacity):
+#     weight = 0
+#     for elem in iterator:
+#         weight += weight_func(elem)
+#         if weight > capacity:
+#             return
+#         yield elem
+
+
+def count_tokens(model, message):
+    total = 0
+    for part in message:
+        tokens = model.tokenize(part["content"].encode("utf-8"))
+        # FIXME: No good way to compute this plus the size added
+        # by chat template stuff
+        total += len(tokens) + 10
+    return total
+
+
+def to_message(page):
+    return [
+        {
+            "type": p["type"],
+            "role": "user" if p["type"] == "draft" else "assistant",
+            "content": p["text"],
+        }
+        for p in page
+    ]
+
+
+def to_full(message):
+    return [p for p in message]
+
+
+def to_medium(message):
+    return [e["content"] if e["type"] != "draft" else "> [...]\n\n" for e in message]
+
+
+def to_minimal(message):
+    return [
+        e["content"] if e["type"] != "draft" else "> [...]\n\n"
+        for e in message
+        if e["type"] != "comment"
+    ]
+
+
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+
+def prompt_pages(pages, model, total):
+    remainder = total
+
+    i = iter(reversed(list([to_message(p) for p in pages])))
+
+    # TODO(bh): write this differently
+
+    try:
+
+        # Allocate 50% of available space to full details (everything included)
+        current_total = 0
+        while True:
+            message = to_full(next(i))
+            size = count_tokens(model, message)
+            if (current_total + size) > (remainder / 2):
+                break
+            yield message
+            current_total += size
+
+        remainder -= current_total
+
+        # Allocate 50% of the remainder to medium details (original text excluded)
+        current_total = 0
+        while True:
+            message = to_medium(next(i))
+            size = count_tokens(model, message)
+            if (current_total + size) > (remainder / 2):
+                break
+            yield message
+            current_total += size
+
+        remainder -= current_total
+
+        # Allocate the remainder to minimal details (original text and comments excluded)
+        while remainder > 0:
+            message = to_minimal(next(i))
+            size = count_tokens(model, message)
+            if size > remainder:
+                break
+            yield message
+            remainder -= size
+    except StopIteration:
+        pass
+
+
+def prepare_prompt(base_prompt, pages, model, n_ctx):
+    n_ctx -= count_tokens(model, [{"content": base_prompt}])
+    return [{"role": "system", "content": base_prompt}] + flatten(
+        reversed(list(prompt_pages(pages, model, n_ctx)))
+    )
+
+
+def format_messages(messages):
+    # TODO(bh): groupby role to avoid repeated headers. Might also make sense
+    # to actually group them coming out of the model, repeated assistant
+    # tokens might affect performance.
+    result = ""
+    for message in messages:
+        if "role" in message:
+            result += message["role"].upper() + ":\n"
+        if "content" in message:
+            result += message["content"] + "\n"
+
+    return result
+
+
+def complete_page(model: Llama, grammar, messages):
+    generated = ""
+
+    print(format_messages(messages))
+
+    for token in model.create_chat_completion(
+        messages,
+        grammar=grammar,
+        stream=True,
+        max_tokens=512,
+    ):
+
+        # rich.print(token)
+        if "content" not in token["choices"][0]["delta"]:
+            rich.print(token)
+            continue
+
+        text = token["choices"][0]["delta"]["content"]
+        generated += text
+
+        # FIXME: Should expect a newline
+        if re.search(r"\$ page \d+\D", generated):
+            rich.print("Found page command", token)
+            break
+
+        print(text, end="")
+        sys.stdout.flush()
+
+    page = []
+
+    for line in generated.split("\n"):
+        if line.startswith("Comment: "):
+            page.append({"type": "comment", "text": line})
+        elif line.startswith("$ "):
+            # page.append({"type": "cmd", "text": line})
+            # TODO: add result of command
+            # For now let's just impute a command
+            pass
+        elif line.startswith("Summary of page "):
+            page.append({"type": "summary", "text": line})
+        elif line.startswith("Note to self:"):
+            page.append({"type": "note", "text": line})
+        else:
+            pass
+
+    return page
 
 
 @click.command()
 @click.option(
     "-m",
     "--model",
+    "model_path",
     type=click.Path(exists=True),
+    required=True,
 )
 @click.option(
     "-t",
     "--template",
     type=click.File("r"),
+    required=True,
 )
 @click.option(
     "-g",
     "--gbnf",
     type=click.File("r"),
+    required=True,
 )
 @click.option(
     "-d",
     "--draft",
     type=click.File("r"),
+    required=True,
 )
 @click.option("-n", "--n_ctx", default=8192, type=int)
-def main(model, template, gbnf, draft, n_ctx):
+def main(model_path, template, gbnf, draft, n_ctx):
+
+    draft_pages = read_draft(draft)
+
+    template_vars = collections.defaultdict(dict)
+    template_vars["ietfcert.pagecount"] = str(len(draft_pages))
+
+    base_prompt = template.read()
+
+    def subst(m: re.Match):
+        return template_vars.get(m.group(1), None) or "null"
+
+    base_prompt = re.sub(r"\{\{(ietfcert\.\w+)\}\}", subst, base_prompt)
+
+    print(base_prompt)
 
     grammar = LlamaGrammar.from_string(gbnf.read())
-    model = Llama(model_path=model, n_ctx=n_ctx)
-    prompt_text = template.read()
+    model = Llama(model_path=model_path, n_ctx=n_ctx)
 
-    seen_command = False
-    seen_junk = False
+    for nth in range(len(draft_pages)):
+        sliced = draft_pages[: nth + 1]
 
-    def stop(tokens, *args, **kwargs):
-        if seen_command and tokens[-1] in [model.token_nl(), model.token_eos()]:
-            return True
-        if seen_junk:
-            return True
-        return False
+        # Right now we ignore commands and just pretend the next page is requested.
+        sliced[-1].insert(0, {"type": "cmd", "text": f"$ page {nth+1}\n"})
+        prompt = prepare_prompt(base_prompt, sliced, model, n_ctx)
 
-    generated = ""
-
-    for token in model.create_completion(
-        prompt_text,
-        grammar=grammar,
-        stream=True,
-        max_tokens=512,
-        stopping_criteria=StoppingCriteriaList([stop]),
-        
-    ):
-        text = token["choices"][0]["text"]
-        generated += text
-
-        for line in generated.split("\n")[:-1]:
-            if line.startswith("Comment: "):
-                pass
-            elif line.startswith("$ "):
-                seen_command = True
-            elif line.startswith("Summary "):
-                pass
-            elif line.startswith("Note to self:"):
-                pass
-            else:
-                seen_junk = True
-
-        print(text, end="")
-        sys.stdout.flush()
+        processed = complete_page(model, grammar, prompt)
+        sliced[-1].extend(processed)
 
 
 if __name__ == "__main__":
